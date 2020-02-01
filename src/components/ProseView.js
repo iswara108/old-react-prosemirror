0c07c2905bc6ee42622e0bfb6be5af257894c4d0
Iswara Chaitanya <iswara@sivananda.org>
2020-01-31 22:49:11 -0500

    inline proseMirrorHooks into ProseView

--- src/components/ProseView.js
+++ src/components/ProseView.js
@@ -1,15 +1,41 @@
-import React, { useEffect } from 'react'
+import React, { useState, useLayoutEffect, useRef } from 'react'
+import { EditorView } from 'prosemirror-view'
 import 'prosemirror-view/style/prosemirror.css'
 
-import { useProseView } from './proseMirrorHooks'
+import './richTextEditor.css'
 
 export default props => {
-  const { dom, editorView } = useProseView(props)
+  const { editorState, autoFocus } = props
+  const [editorView, setEditorView] = useState(null)
 
-  useEffect(() => {
-    if (props.setEditorView) props.setEditorView(editorView)
-  }, [editorView, props])
+  const dom = useRef(document.createElement('div'))
 
+  // Initialize editor view on the first time the state exists
+  useLayoutEffect(() => {
+    if (editorState && !editorView) {
+      setEditorView(
+        new EditorView(dom.current, {
+          state: editorState
+        })
+      )
+    }
+  }, [editorState, editorView])
+
+  // Update view whenever the state changes
+  useLayoutEffect(() => {
+    if (editorView && editorState) {
+      if (!editorView.state !== editorState) {
+        editorView.updateState(editorState)
+      }
+    }
+  }, [editorState, editorView])
+
+  // if autoFocus applies - focus upon the creation of the view
+  useLayoutEffect(() => {
+    if (editorView && autoFocus) editorView.focus()
+  }, [editorView, autoFocus])
+
+  // remove non HTML props
   const propsToDiv = { ...props }
   delete propsToDiv.editorState
   delete propsToDiv.setEditorView
